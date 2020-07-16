a507d35fadb10637638c95cf21d9001727af1a5d
Bhoomika Mewada <bhoomikamewada@gmail.com>
2020-07-16 12:22:05 +0530

    assignment4

---
+++ python_django4/My-portfolio-website-master/my_envs/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\web development certification course\web_development\python_django4\my-portfolio-website-master\my_envs\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
